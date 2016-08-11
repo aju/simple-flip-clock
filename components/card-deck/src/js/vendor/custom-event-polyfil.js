f598786c6b20e4a3c201525a4f116881067acce3
Konrad Dzwinel <kdzwinel@gmail.com>
2016-08-11 09:50:15 +0200

    Card Deck: sending an event whenever card is discarded

---
+++ components/card-deck/src/js/vendor/custom-event-polyfil.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+function CustomEvent(event, params) {
+  params = params || {bubbles: false, cancelable: false, detail: undefined};
+  var evt = document.createEvent('CustomEvent');
+  evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+  return evt;
+}
+
+CustomEvent.prototype = window.Event.prototype;
+
+export default (typeof window.CustomEvent === "function") ? window.CustomEvent : CustomEvent;
