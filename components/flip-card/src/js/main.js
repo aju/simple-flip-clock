962d643bd3d26608c908b78d44b9519e1ed3b10f
Konrad Dzwinel <kdzwinel@gmail.com>
2016-08-17 12:50:35 +0200

    Make linter happy

--- components/flip-card/src/js/main.js
+++ components/flip-card/src/js/main.js
@@ -1,19 +1,20 @@
 class FlipCard {
   /**
-   * @param {!Node} element
-   * @param {?boolean} vertical
-     */
-  constructor({element, vertical=false}) {
+   * @param {!Node} element - parent node
+   * @param {?boolean} vertical - controls if animation should be vertical or horizontal
+   */
+  constructor({element, vertical = false}) {
     this.element = element;
     this.vertical = !!vertical;
 
-    if(vertical) {
+    if (vertical) {
       this.element.classList.add('fc-box--vertical');
     }
   }
 
   flip() {
     const flipClass = this.vertical ? 'fc-box--flipped-vertically' : 'fc-box--flipped';
+
     this.element.classList.toggle(flipClass);
   }
 }
