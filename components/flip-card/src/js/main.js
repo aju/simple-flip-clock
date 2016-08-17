7880689a0f529c230d2b5378bc793ab99e4be3c1
Konrad Dzwinel <kdzwinel@gmail.com>
2016-08-17 12:46:40 +0200

    Flip box: add vertical flip

--- components/flip-card/src/js/main.js
+++ components/flip-card/src/js/main.js
@@ -1,9 +1,20 @@
 class FlipCard {
-  constructor({element}) {
+  /**
+   * @param {!Node} element
+   * @param {?boolean} vertical
+     */
+  constructor({element, vertical=false}) {
     this.element = element;
+    this.vertical = !!vertical;
+
+    if(vertical) {
+      this.element.classList.add('fc-box--vertical');
+    }
   }
+
   flip() {
-    this.element.classList.toggle('fc-box--flipped');
+    const flipClass = this.vertical ? 'fc-box--flipped-vertically' : 'fc-box--flipped';
+    this.element.classList.toggle(flipClass);
   }
 }
 
